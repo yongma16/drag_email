d98d82881e7aa4160db457937a47141112f037ab
yma16 <17685043634@163.com>
2025-02-05 15:12:56 +0800

    feat: init

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 // @ts-ignore
-import React from 'react';
+import React from "react";
 // @ts-ignore
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
