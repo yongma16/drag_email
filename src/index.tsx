f711442fd4410379ede07f54e4c9d7535c856dcf
yma16 <17685043634@163.com>
2025-02-06 17:17:50 +0800

    feat: less

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-
+import "./style/index.less";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
