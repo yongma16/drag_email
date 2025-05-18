5ddb997570e03ded5c32e75d123c7d43bc86ff09
yma16 <17685043634@163.com>
2025-05-18 23:46:48 +0800

    fix: style

---
+++ src/components/button/index.tsx
@@ -0,0 +1,9 @@
+const Button = (props: any) => {
+  return (
+    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      button
+    </button>
+  );
+};
+
+export default Button;
